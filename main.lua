dc49c42c8f721e9caa01e5f8c5c1e8a0fe99babc
Ian H. Bateman <ianhenrybateman@gmail.com>
2018-01-21 16:03:08 -0500

    got basic animation working

--- main.lua
+++ main.lua
@@ -1,18 +1,32 @@
--- Load some default values for our rectangle.
 function love.load()
-    x, y, w, h = 20, 20, 60, 20
+    animation = newAnimation(love.graphics.newImage("oldHero.png"), 16, 18, 1)
 end
  
--- Increase the size of the rectangle every frame.
 function love.update(dt)
-	if w < 500 then
-		w = w + 1
-		h = h + 1
-	end
+    animation.currentTime = animation.currentTime + dt
+    if animation.currentTime >= animation.duration then
+        animation.currentTime = animation.currentTime - animation.duration
+    end
 end
  
--- Draw a coloured rectangle.
 function love.draw()
-    love.graphics.setColor(0, 100, 100)
-    love.graphics.rectangle("fill", x, y, w, h)
+    local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum], 0, 0, 0, 4)
+end
+ 
+function newAnimation(image, width, height, duration)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
+ 
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+ 
+    animation.duration = duration or 1
+    animation.currentTime = 0
+ 
+    return animation
 end
